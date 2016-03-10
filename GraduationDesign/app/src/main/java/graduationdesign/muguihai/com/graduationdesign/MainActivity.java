f333922776085a0c9c740814e0d47c61cd6190a7
wapalxj <wapalxj@163.com>
2016-03-10 19:37:17 +0800

    16.03.10

---
+++ GraduationDesign/app/src/main/java/graduationdesign/muguihai/com/graduationdesign/MainActivity.java
@@ -0,0 +1,16 @@
+package graduationdesign.muguihai.com.graduationdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
